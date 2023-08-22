5f35e10db510739f58df2bca5f78a3521e43f217
simeydotme <simey.me@gmail.com>
2023-08-23 01:52:42 +0800

    add disabled sliders examples

---
+++ src/components/Demos/examples/disabled/Disabled.svelte
@@ -0,0 +1,14 @@
+<script>
+  import RangeSlider from 'svelte-range-slider-pips';
+  let values = [50];
+  let disabled = false;
+  let toggle = () => disabled = !disabled;
+</script>
+
+<RangeSlider {disabled} bind:values />
+
+<button type="button" on:click={toggle}>
+  {disabled ? 'Enable' : 'Disable'}
+</button>
+
+<code data-values title="The output slider values">{disabled} | {values}</code>
---
+++ src/components/Demos/examples/disabled/DisabledBound.svelte
@@ -0,0 +1,17 @@
+<script>
+  import RangeSlider from 'svelte-range-slider-pips';
+  let values = [25,75];
+</script>
+
+<RangeSlider disabled range float bind:values />
+
+<input type="number" bind:value={values[0]}>
+<input type="number" bind:value={values[1]}>
+
+<code data-values title="The output slider values">{values}</code>
+
+<style hide>
+  input[type=number] {
+    width: 8em;
+  }
+</style>
