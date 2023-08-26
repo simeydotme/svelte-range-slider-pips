bd6e0b20fb273493307047fd29418cfee89d9196
simeydotme <simey.me@gmail.com>
2023-08-27 02:42:47 +0800

    add reversed

---
+++ src/components/Demos/examples/reversed/Reversed.svelte
@@ -0,0 +1,8 @@
+<script>
+  import RangeSlider from 'svelte-range-slider-pips';
+  let values = [30,70];
+</script>
+
+<RangeSlider reversed pips all="label" float range bind:values pipstep={20} />
+
+<code data-values title="The output slider values">{values}</code>
---
+++ src/components/Demos/examples/reversed/ReversedVertical.svelte
@@ -0,0 +1,32 @@
+<script>
+  import RangeSlider from 'svelte-range-slider-pips';
+  let values = [0,60];
+</script>
+
+<div data-grid>
+
+  <p>Normal</p>
+  <RangeSlider vertical pips all="label" float range bind:values pipstep={20} />
+
+  <p>Reversed</p>
+  <RangeSlider reversed vertical pips all="label" float range bind:values pipstep={20} />
+
+</div>
+
+<code data-values title="The output slider values">{values}</code>
+
+
+<style hide>
+  [data-grid] {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 10px;
+    justify-items: center;
+  }
+  [data-grid] :global(.rangeSlider) {
+    margin: 2em 0 1em -2em!important;
+  }
+  [data-grid] p {
+    grid-row: 2;
+  }
+</style>
