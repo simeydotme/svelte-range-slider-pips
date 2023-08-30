e5900acea932f9ca60ad9cef8c8778bf9235041a
simeydotme <simey.me@gmail.com>
2023-08-31 03:24:56 +0800

    improve price gradient recipie

--- src/components/Demos/recipes/pricegradient/PriceGradient.svelte
+++ src/components/Demos/recipes/pricegradient/PriceGradient.svelte
@@ -5,17 +5,51 @@
   /* hide */
   const renderCss = `<style>${css}</style>`;
   /* endhide */
+  
+  let slider;
+  let timer;
+  let values = [ 2333, 7878 ];
 
   const currency = new Intl.NumberFormat( "en", { style: 'currency', currency: 'USD', maximumFractionDigits: 0 });
   const formatter = (value) => currency.format(value);
-  let values = [ 2333, 7878 ];
 
+  const stop = () => {
+    slider.classList.remove( "up", "down" );
+  }
+
+  const slide = (e) => {
+    const delta = -(e.detail.previousValue - e.detail.value);
+    if ( delta > 0 ) {
+      slider.classList.add( "up" );
+      slider.classList.remove( "down" );
+    } else {
+      slider.classList.add( "down" );
+      slider.classList.remove( "up" );
+    }
+    clearTimeout( timer );
+    timer = setTimeout( stop, 66 );
+  }
 
 </script>
 
+<RangeSlider 
+  id="PriceGradient" 
+  bind:slider
+  bind:values
+  range
+  float
+  pips
+  first={false}
+  last={false}
+  min={0}
+  max={10000}
+  step={1}
+  pipstep={200}
+  {formatter}
+  on:change={slide}
+  on:stop={stop} 
+/>
 
-<RangeSlider id="PriceGradient" bind:values range float min={0} max={10000} step={1} {formatter} />
-
-
-
+<!-- hide -->
 {@html renderCss}
+<!-- endhide -->
