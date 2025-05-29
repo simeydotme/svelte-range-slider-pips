6fe6c031a0cdd887dd536802f1de7d6da2e68154
simeydotme <simey.me@gmail.com>
2025-05-29 22:56:59 +0800

    remove minifier?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import mdx from '@astrojs/mdx';
 // import preact from '@astrojs/preact';
 // import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import minifier from 'astro-html-minifier';
 import expressiveCode from "astro-expressive-code";
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), mdx(), minifier(), sitemap()],
+  integrations: [svelte(), mdx(), sitemap()],
   base: `/svelte-range-slider-pips/`,
   site: `https://simeydotme.github.io`,
   outDir: `./dist`,
