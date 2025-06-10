3b7ec28533fb8a3b449435e9f26fdb9c020c6a47
simeydotme <simey.me@gmail.com>
2025-06-10 14:28:24 +0800

    add auto positioning example

---
+++ src/components/Demos/examples/float/AutoPosition.svelte
@@ -0,0 +1,42 @@
+<script>
+  import RangeSlider from 'svelte-range-slider-pips';
+  import css from './AutoPosition.css?inline';
+
+  /* hide */
+  const renderCss = `<style>${css}</style>`;
+  /* endhide */
+  let value = 80;
+
+</script>
+
+<div class="container">
+
+  <RangeSlider 
+    id="auto-position"
+    bind:value 
+    float
+    handleFormatter={value => `Stays Inside Container @ ${(value * -0.6 - 20).toFixed(1)}%`}
+  />
+
+  <p>Move the handle from left-right</p>
+
+  <RangeSlider 
+    bind:value 
+    float
+    handleFormatter={value => `Overflows Container @ ${value}%`}
+  />
+
+</div>
+
+<code data-values title="The output slider values">{value}</code>
+
+<!-- hide -->
+{@html renderCss}
+<!-- endhide -->
+
+<style>
+  :global(.container .rangeFloat) {
+    opacity: 1;
+    translate: -50% -20% 0.01px;
+  }
+</style>
--- src/components/Demos/examples/float/ToggleRangeFloat.svelte
+++ src/components/Demos/examples/float/ToggleRangeFloat.svelte
@@ -1,15 +1,26 @@
 <script>
   import RangeSlider from 'svelte-range-slider-pips';
+  import css from './ToggleRangeFloat.css?inline';
+
+  /* hide */
+  const renderCss = `<style>${css}</style>`;
+  /* endhide */
   let values = [25,75];
+
 </script>
 
 <RangeSlider 
+  id="toggle-range-float"
   bind:values 
   range
-  rangeFloat={values[1] - values[0] < 30} 
-  float={values[1] - values[0] >= 30} 
+  rangeFloat={values[1] - values[0] < 22} 
+  float={values[1] - values[0] >= 22} 
 />
 
-<p>Try dragging the handles to see the floats toggle</p>
+<p>Try dragging the handles close to each other</p>
 
 <code data-values title="The output slider values">{values}</code>
+
+<!-- hide -->
+{@html renderCss}
+<!-- endhide -->
